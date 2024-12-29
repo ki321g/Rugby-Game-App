6ac1c2c4aa9644bcbd40aaeebff5c06c60b3b463
Kieron Garvey <garveykieron@gmail.com>
2024-12-29 12:09:02 +0000

    Removed API & ROOM files

--- app/src/main/java/ie/setu/rugbygame/data/api/ServiceEndPoints.kt
+++
@@ -1,8 +0,0 @@
-package ie.setu.rugbygame.data.api
-
-class ServiceEndPoints {
-    companion object {
-        const val BASE_URL= "https://donationxweb-multi-server.onrender.com/"
-        const val DONATIONS_ENDPOINT = "donations"
-    }
-}
